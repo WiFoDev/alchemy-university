9f14592856a1d21bd79ca8a67f89eb8e524b7b8a
WiFoDev <lou81804@gmail.com>
2022-10-14 19:26:00 -0500

    challenge4: add fetch nfts functionalities

--- challenge4/nft-explorer/components/CollectionForm.jsx
+++ challenge4/nft-explorer/components/CollectionForm.jsx
@@ -1,42 +1,43 @@
 import React, { useState } from 'react'
 
-export const CollectionForm = () => {
+export const CollectionForm = ({ fetchNFTs, fetchNFTsForCollection }) => {
   const [walletAddress, setWalletAddress] = useState('')
   const [collectionAddress, setCollectionAddress] = useState('')
-  const [isCollectionInputDisable, setCollectionInputDisable] = useState(true)
+  const [isWalletInputDisabled, setWalletInputDisabled] = useState(false)
 
-  const fetchNFTs = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault()
-    console.log({
-      walletAddress,
-      collectionAddress
-    })
+    if (!isWalletInputDisabled) {
+      fetchNFTs(walletAddress.trim(), collectionAddress.trim())
+    } else {
+      fetchNFTsForCollection(collectionAddress.trim())
+    }
   }
 
   return (
     <section className="min-h-[33.33%] grid place-items-center">
-      <form onSubmit={fetchNFTs} className='w-1/2 flex flex-col gap-2 items-center'>
+      <form onSubmit={handleSubmit} className='w-1/2 flex flex-col gap-2 items-center'>
         <input
-        className='bg-[#f1f5f9] w-full p-1 rounded outline-[#92c5fd]'
+        className={`bg-[#f1f5f9] w-full p-1 rounded outline-[#92c5fd] ${isWalletInputDisabled ? 'cursor-not-allowed' : ''}`}
         type="text"
         placeholder='Add your wallet address'
         value={walletAddress}
         onChange={(e) => setWalletAddress(e.target.value)}
+        disabled={isWalletInputDisabled}
         />
         <input
-        className={`bg-[#f1f5f9] w-full p-1 rounded outline-[#92c5fd] ${isCollectionInputDisable ? 'cursor-not-allowed' : ''}`}
+        className='bg-[#f1f5f9] w-full p-1 rounded outline-[#92c5fd]'
         type="text"
         placeholder='Add the collection address'
         value={collectionAddress}
         onChange={(e) => setCollectionAddress(e.target.value)}
-        disabled={isCollectionInputDisable}
         />
         <div className='flex items-center justify-center gap-1'>
           <input
             type="checkbox"
             id='fetchCollection'
-            checked={!isCollectionInputDisable}
-            onChange={() => setCollectionInputDisable(prev => !prev) }
+            checked={isWalletInputDisabled}
+            onChange={() => setWalletInputDisabled(prev => !prev) }
           />
           <label htmlFor="fetchCollection">Fetch for collection</label>
         </div>
--- challenge4/nft-explorer/pages/index.js
+++ challenge4/nft-explorer/pages/index.js
@@ -1,10 +1,34 @@
+import { useState } from 'react'
 import { CollectionForm } from '../components/CollectionForm'
 import { Layout } from '../components/Layout'
 
+const apiKey = process.env.NEXT_PUBLIC_ALCHEMY_API_KEY
+const baseURL = `https://eth-mainnet.g.alchemy.com/nft/v2/${apiKey}`
+
 export default function Home () {
+  const [NFTs, setNFTs] = useState([])
+
+  const fetchNFTs = async (owner, collection) => {
+    let endpoint = `${baseURL}/getNFTs?owner=${owner}`
+    if (collection.length) {
+      endpoint = `${endpoint}&contractAddresses%5B%5D=${collection}`
+    }
+    const { ownedNfts } = await fetch(endpoint).then(res => res.json())
+    setNFTs(ownedNfts)
+  }
+
+  const fetchNFTsForCollection = async (collection) => {
+    const endpoint = `${baseURL}/getNFTsForCollection?contractAddress=${collection}&withMetadata=true`
+    const { nfts } = await fetch(endpoint).then(res => res.json())
+    setNFTs(nfts)
+  }
+  console.log(NFTs)
   return (
     <Layout >
-      <CollectionForm />
+      <CollectionForm
+        fetchNFTs={fetchNFTs}
+        fetchNFTsForCollection={fetchNFTsForCollection}
+      />
     </Layout>
   )
 }
