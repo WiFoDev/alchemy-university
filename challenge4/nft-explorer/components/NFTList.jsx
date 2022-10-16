5883d164c9ff9f908fd3d5ce1ae6eefd00fa53c6
WiFoDev <lou81804@gmail.com>
2022-10-16 11:21:40 -0500

    challenge4: add navigation styling

--- challenge4/nft-explorer/components/NFTList.jsx
+++ challenge4/nft-explorer/components/NFTList.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { NextArrowIcon } from './NextArrowIcon'
 import { NFTItem } from './NFTItem'
 
 export const NFTList = ({ nfts, nextPage, changePage }) => {
   return (
     <>
-      {nextPage && <a className='underline cursor-pointer' onClick={changePage}>Next Page</a>}
+      <div className='flex justify-end mb-4'>
+        {nextPage && <div className='cursor-pointer w-7 h-7 shadow-gray-600/70 shadow-md rounded-full p-1 hover:shadow-black hover:shadow-md' onClick={changePage}><NextArrowIcon /></div>}
+      </div>
       <section className="grid grid-cols-6 gap-6 mb-4">
         {nfts.length > 0 &&
           nfts.map((nft, index) => {
