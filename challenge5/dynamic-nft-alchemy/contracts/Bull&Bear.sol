e9d1c8481f8d152fa4f4603de48d2fdf34c6a016
WiFoDev <lou81804@gmail.com>
2022-10-19 12:54:50 -0500

    challenge5: add chainlink aggregator to smart contract

--- challenge5/dynamic-nft-alchemy/contracts/Bull&Bear.sol
+++ challenge5/dynamic-nft-alchemy/contracts/Bull&Bear.sol
@@ -8,12 +8,27 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+
 import "hardhat/console.sol";
 
-contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+contract BullBear is
+    ERC721,
+    ERC721Enumerable,
+    ERC721URIStorage,
+    Ownable,
+    KeeperCompatibleInterface
+{
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    AggregatorV3Interface public pricefeed;
+
+    uint public interval;
+    uint public lastTimeStamp;
+
+    int public currentPrice;
 
     string[] bullUrisIpfs = [
         "https://ipfs.io/ipfs/QmRXyfi3oNZCubDxiVFre3kLZ8XeGt6pQsnAQRZ7akhSNs?filename=gamer_bull.json",
@@ -26,7 +41,32 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
         "https://ipfs.io/ipfs/QmbKhBXVWmwrYsTPFYfroR2N7NAekAMxHUVg2CWks7i9qj?filename=simple_bear.json"
     ];
 
-    constructor() ERC721("Bull&Bear", "BBTK") {}
+    event TokensUpdate(string marketTrend);
+
+    constructor(uint updateInterval, address _priceFeed)
+        ERC721("Bull&Bear", "BBTK")
+    {
+        interval = updateInterval;
+        lastTimeStamp = block.timestamp;
+
+        pricefeed = AggregatorV3Interface(_priceFeed);
+
+        currentPrice = getLatestPrice();
+    }
+
+    function getLatestPrice() public view returns (int) {
+        (
+            ,
+            /*uint80 roundID*/
+            int price, /*uint startedAt*/
+            ,
+            ,
+
+        ) = /*uint timeStamp*/
+            /*uint80 answeredInRound*/
+            pricefeed.latestRoundData();
+        return price;
+    }
 
     function safeMint(address to) public {
         uint256 tokenId = _tokenIdCounter.current();
@@ -44,6 +84,64 @@ contract BullBear is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
             " and assigned token url: ",
             uri
         );
+    }
+
+    function checkUpkeep(bytes calldata)
+        external
+        view
+        override
+        returns (bool upkeepNeeded, bytes memory)
+    {
+        upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
+    }
+
+    function performUpkeep(bytes calldata) external override {
+        if ((block.timestamp - lastTimeStamp) > interval) {
+            lastTimeStamp = block.timestamp;
+            int latestPrice = getLatestPrice();
+
+            if (latestPrice == currentPrice) {
+                console.log("NO CHANGE => Returning!");
+                return;
+            }
+
+            if (latestPrice < currentPrice) {
+                console.log("ITS BEAR TIME");
+                updateAllTokensURI("bear");
+            }
+        }
+    }
+
+    function updateAllTokensURI(string memory trend) internal {
+        if (compareStrings("bear", trend)) {
+            console.log("UPDATING TOKENS URI WITH ", trend, " trend");
+            for (uint i = 0; i < _tokenIdCounter.current(); i++) {
+                _setTokenURI(i, bearUrisIpfs[0]);
+            }
+        } else {
+            console.log("UPDATING TOKENS URI WITH ", trend, " trend");
+            for (uint i = 0; i < _tokenIdCounter.current(); i++) {
+                _setTokenURI(i, bullUrisIpfs[1]);
+            }
+        }
+        emit TokensUpdate(trend);
+    }
+
+    function setPriceFeed(address newFeed) public onlyOwner {
+        pricefeed = AggregatorV3Interface(newFeed);
+    }
+
+    function setInterval(uint256 newInterval) public onlyOwner {
+        interval = newInterval;
+    }
+
+    function compareStrings(string memory a, string memory b)
+        internal
+        pure
+        returns (bool)
+    {
+        return (keccak256(abi.encodePacked(a)) ==
+            keccak256(abi.encodePacked(b)));
     }
 
     // The following functions are overrides required by Solidity.
